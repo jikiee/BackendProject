24d6e31018177536f3ee7436e8c3e4250f8478a5
jikie444 <444orhun@gmail.com>
2022-03-24 21:28:18 +0300

    Added data access and business codes for product class and added entity framework

---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,34 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        IProductDal _productDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public List<Product> GetAll()
+        {
+            return _productDal.GetAll();
+        }
+
+        public List<Product> GetByCategoryId(int categoryId)
+        {
+            return _productDal.GetAll(p => p.CategoryId == categoryId);
+        }
+
+        public Product GetById(int id)
+        {
+            return _productDal.Get(p => p.ProductId == id);
+        }
+    }
+}
---
+++ DataAccess/Abstract/IProductDal.cs
@@ -0,0 +1,17 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    public interface IProductDal
+    {
+        List<Product> GetAll(Expression<Func<Product, bool>> filter = null);
+        Product Get(Expression<Func<Product, bool>> filter);
+        void Add(Product product);
+        void Update(Product product);
+        void Delete(Product product);
+    }
+}
